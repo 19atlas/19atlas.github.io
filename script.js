9c77258a6e72fd7cade136218374b459ad44f863
19atlas <o.ustuncelik@gmail.com>
2023-10-08 17:28:36 +0300

    add image

--- script.js
+++ script.js
@@ -1,46 +1,14 @@
-const projectContents = document.querySelectorAll('.project-content');
-
-const prevButton = document.getElementById('prev-button');
-const nextButton = document.getElementById('next-button');
-let currentProject = 0;
-
-function showProject(projectIndex) {
-    projectContents.forEach((project, index) => {
-        project.classList.remove('active');
-        if (index === projectIndex) {
-            project.classList.add('active');
-        }
-    });
-}
-
-prevButton.addEventListener('click', () => {
-    currentProject = (currentProject - 1 + projectContents.length) % projectContents.length;
-    showProject(currentProject);
-});
-
-nextButton.addEventListener('click', () => {
-    currentProject = (currentProject + 1) % projectContents.length;
-    showProject(currentProject);
-});
-
 document.addEventListener('DOMContentLoaded', function () {
     const projectContents = document.querySelectorAll('.project-content');
     const prevButton = document.getElementById('prev-button');
     const nextButton = document.getElementById('next-button');
     let currentProject = 0;
 
-    const projectBackgrounds = [
-        'url("")',
-        'url("")',
-        'url("")'
-    ];
-
     function showProject(projectIndex) {
         projectContents.forEach((project, index) => {
             project.classList.remove('active');
             if (index === projectIndex) {
                 project.classList.add('active');
-                document.body.style.backgroundImage = projectBackgrounds[index];
             }
         });
     }
